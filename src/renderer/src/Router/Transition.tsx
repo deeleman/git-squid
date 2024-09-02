289bed28b37cf73ffa0a72f5f35289e3194983f8
Pablo Deeleman <deeleman@gmail.com>
2024-09-03 00:28:40 +0200

    feat: Add enter transition to routed elements and JSDoc annotations to Router

---
+++ src/renderer/src/Router/Transition.tsx
@@ -0,0 +1,18 @@
+import { styled } from '@twilio-paste/core/styling-library'
+import { Box } from '@twilio-paste/core/box'
+
+/**
+ * Animation wrapper to provide a smooth enter transition to any contained element upon mounting.
+ */
+export const Transition = styled(Box)`
+  @keyframes fadeIn {
+    0% {
+      opacity: 0.4;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+
+  animation: fadeIn 0.6s normal forwards;
+`
--- src/renderer/src/Router/index.tsx
+++ src/renderer/src/Router/index.tsx
@@ -1,13 +1,30 @@
 import { createContext, PropsWithChildren, useCallback, useContext, useRef, useState } from 'react'
+import { Transition } from './Transition'
 
+const DEFAULT_ROUTE = 'index'
+
+/**
+ * Router entities handled by the `<Router />` underlying context.
+ */
 type RouterContextApi = {
+  /**
+   * Redirects the user to the component wrapped by the `<Route>` element annotated with such path.
+   * @param path The path whose/s component/s we want to render. Admits full URLs.
+   */
   navigate: (path: string) => void
+  /**
+   * Current route enabled in the system. Defaults to {@link DEFAULT_ROUTE}.
+   */
   currentRoute: string
 }
 
-const DEFAULT_ROUTE = 'index'
 const RouterContext = createContext<RouterContextApi | undefined>(undefined)
 
+/**
+ * The `<Router>` element, used in combination with its `<Route>` child node, provides
+ * a convenient way to emulate string-based routing in the renderer application. If
+ * the path passed is a full URL, the router will redirect the user to such URL instead.
+ */
 export function Router(props: PropsWithChildren): JSX.Element {
   const [currentRoute, setCurrentRoute] = useState(DEFAULT_ROUTE)
   const externalRouteRegexRef = useRef(new RegExp(/^https?:/))
@@ -30,7 +47,17 @@ export function Router(props: PropsWithChildren): JSX.Element {
   )
 }
 
-export function Route(props: PropsWithChildren<{ path?: string }>): JSX.Element | null {
+/**
+ * Meant to be used as a child node of {@link Router}, decorated with a `path` property.
+ */
+export function Route(
+  props: PropsWithChildren<{
+    /**
+     * Path to redirect the user to. Admits full URLs.
+     */
+    path?: string
+  }>
+): JSX.Element | null {
   const context = useContext(RouterContext)
   const path = props.path || DEFAULT_ROUTE
 
@@ -38,9 +65,14 @@ export function Route(props: PropsWithChildren<{ path?: string }>): JSX.Element 
     throw new Error('The <Route> component must be instantiated inside a <Router> element.')
   }
 
-  return path === context.currentRoute ? <>{props.children}</> : null
+  return path === context.currentRoute ? <Transition>{props.children}</Transition> : null
 }
 
+/**
+ * A companion hook to interact with the routing mechanism from any component.
+ * @returns A {@link RouterContextApi} object featuring the current route and
+ * a `navigate()` handler to toggle the current component.
+ */
 export function useRouter(): RouterContextApi {
   const context = useContext(RouterContext)
 
